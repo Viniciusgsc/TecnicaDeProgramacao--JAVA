06bc31c4fd529f4c64b936bb90bc9bf620dea159
Viniciusgsc <88463035+Viniciusgsc@users.noreply.github.com>
2022-08-19 20:18:30 -0300

    "Iniciando em JAVA"
    "Iniciando em JAVA"

---
+++ projeto001/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // idade ano atual
+        int idade = 35;
+        // idade ano que vem
+        int idadeAnoQueVem = idade+1;
+        System.out.println("Proxima idade:" + idadeAnoQueVem);
+        System.out.println("idade: " + idade);
+    }
+}
