63adb3192af2d60ad21f6471e5625ba5b63170a5
Viniciusgsc <88463035+Viniciusgsc@users.noreply.github.com>
2022-08-26 19:38:12 -0300

    "Exemplos Java"

---
+++ projeto001/src/Exemplo003.java
@@ -0,0 +1,8 @@
+public class Exemplo003 {
+    public static void name() {
+      int vetor[] = {5,6,8,10,13};
+      boolean achou = false;
+      for (int i = 0; !achou && i < vetor.length; i++);
+    }
+    
+}
---
+++ projeto001/src/Exemplo004.java
@@ -0,0 +1,10 @@
+public class Exemplo004 {
+    public static void name() {
+        for (int i = 0; i<10; i++){
+            if (i==6){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
