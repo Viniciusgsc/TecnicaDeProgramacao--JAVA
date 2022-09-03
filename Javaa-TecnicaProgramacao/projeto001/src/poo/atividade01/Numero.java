4631e706bbd111a134834e1776a7ce11e9762586
Vinícius Gabriel Da Silva Caetano <88463035+Viniciusgsc@users.noreply.github.com>
2022-09-02 21:57:52 -0300

    "Atividade"

---
+++ Javaa-TecnicaProgramacao/projeto001/src/poo/atividade01/Numero.java
@@ -0,0 +1,12 @@
+package poo.atividade01;
+
+public class Numero {
+    public static void main(String[] args) {
+        // atributo para representar o valor deste objeto
+        // private int x ==== 4
+        // somar(tipo valor) ==== somar(7) ---> x=4+7
+        // subtratir(tipo valor) 
+        //dividir(tipo valor)
+        //multiplicar(tipo valor)
+    }
+}
