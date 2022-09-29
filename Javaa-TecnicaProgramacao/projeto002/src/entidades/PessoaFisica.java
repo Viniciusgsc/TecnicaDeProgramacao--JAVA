9262e1fd620d9b0f374af3e90a0acc176213cc99
Viniciusgsc <vinycimgsc2020@gmail.com>
2022-09-29 00:01:12 -0300

    "(Formatando Json)"
    "(Formatando Json)"

--- Javaa-TecnicaProgramacao/projeto002/src/entidades/PessoaFisica.java
+++ Javaa-TecnicaProgramacao/projeto002/src/entidades/PessoaFisica.java
@@ -45,10 +45,10 @@ public class PessoaFisica {
     }
 
     public String toString(){
-        return "Nome: " + getNome() + "\nEndereco: " + getEndereco() +
-        "\nContato: " + getContato() +
-        "\nDocumento: " + getDocumento() +
-        "\nData de Nascimento: " + getDataNascimento();
+            return "{'Nome': '" + getNome() +"',"+ "\n'Endereco': '" + getEndereco() +"',"+
+        "\n'Contato': '" + getContato()+"',"+
+        "\n'Documento': '" + getDocumento()+"',"+
+        "\n'Data de Nascimento': '" + getDataNascimento()+"',";
     }
     
 }
